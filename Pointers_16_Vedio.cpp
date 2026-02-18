6433610076cd61a4c08503bf3dc114914018de8c
YoungDevelopers3 <deepakbhatti889@gmail.com>
2026-02-18 03:25:17 -0500

    double pointer added

--- Pointers_16_Vedio.cpp
+++ Pointers_16_Vedio.cpp
@@ -4,9 +4,11 @@ int main()
 {
     int var = 8;
     int *ptr = &var;
+    int **ptr1 = &ptr;
     cout << "The value is " << var << endl;
     cout << "The address of this variable in the memory is " << ptr << endl;
     var = 10;
     cout << "The address of the variable after changing the value is " << ptr << endl;
-    cout << "So the memory address does not changes with the change in its value";
+    cout << "So the memory address does not changes with the change in its value" << endl;
+    cout << "This is a double pointer" << ptr1 << endl;
 }
