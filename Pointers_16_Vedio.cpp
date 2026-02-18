c2b31fb06d88c8a6a730e86c3914c1cb5b968104
YoungDevelopers3 <deepakbhatti889@gmail.com>
2026-02-18 02:58:33 -0500

    Pointers

---
+++ Pointers_16_Vedio.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int var = 8;
+    int *ptr = &var;
+    cout << "The value is " << var << endl;
+    cout << "The address of this variable in the memory is " << ptr << endl;
+    var = 10;
+    cout << "The address of the variable after changing the value is " << ptr << endl;
+    cout << "So the memory address does not changes with the change in its value";
+}
